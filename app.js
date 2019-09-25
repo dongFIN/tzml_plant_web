953f19114334bde502ebdb7ecf0e1c6e49839802
winnie <winnie94528@gmail.com>
2019-09-26 00:50:40 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// 得知螢幕height
+var windowH = $(window).height()
+console.log(windowH)
+$("#homeLogo").height(windowH)
+
+var startPlay = 0
+//判定是否已滑過一個螢幕高
+$(window).scroll(function(event){
+    if($(this).scrollTop() > windowH){
+        console.log($(this).scrollTop())
+        $("div#title_fixed").addClass('titleFixed')
+    }
+    else{
+        $("div#title_fixed").removeClass('titleFixed')
+    }
+    if($(this).scrollTop()>1800 && startPlay == 0){
+        console.log(0)
+        startPlay = 1
+        $("#video_play").attr("src","https://www.youtube.com/embed/2zSx_CGouy4?rel=0&autoplay=1")
+    }
+})
